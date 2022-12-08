c7c58b835ce1708b44d8a3476a29d090fdb85356
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-08 22:20:43 +0100

    definimos mediante while movimientos arriba y abaj

--- recorrido-lab.py
+++ recorrido-lab.py
@@ -1,3 +1,11 @@
 def recorre_laberinto(laberinto):
     columna = fila = 0 #Coordenadas iniciales
     n = 5
+    movimientos = ['Abajo']
+    while (fila < n-1 and columna < n-1):
+        if movimientos[-1] != 'Arriba' and fila + 1 < n and laberinto[fila + 1][columna] != 'X':
+            fila += 1
+            movimientos.append('Abajo')
+        elif movimientos[-1] != 'Abajo' and fila - 1 > 0 and laberinto[fila - 1][columna] != 'X':
+            fila -= 1
+            movimientos.append('Arriba')
